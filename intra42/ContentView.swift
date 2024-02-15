363c397f57291cf295dafcc9a16db517b3a32eef
smickael <mickael.k.swaminathan@gmail.com>
2024-02-15 22:48:48 +0100

    Initial Commit

---
+++ intra42/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  intra42
+//
+//  Created by Mickaël on 15/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ intra42/intra42App.swift
@@ -0,0 +1,17 @@
+//
+//  intra42App.swift
+//  intra42
+//
+//  Created by Mickaël on 15/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct intra42App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
