32c905d9f81f500d64aa2e8d807e0085bd52d325
smickael <mickael.k.swaminathan@gmail.com>
2024-06-29 20:03:27 +0200

    Added SkillsChart.

---
+++ intra42/Components/SkillsChart.swift
@@ -0,0 +1,18 @@
+//
+//  SkillsChart.swift
+//  intra42
+//
+//  Created by Mickaël on 29/06/2024.
+//
+
+import SwiftUI
+
+struct SkillsChart: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SkillsChart()
+}
