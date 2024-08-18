db6bf2a6b88ef7734d89437b7cce922efbe74fad
smickael <mickael.k.swaminathan@gmail.com>
2024-08-18 18:23:50 +0200

    Added Loading Spinner

---
+++ intra42/Components/Spinner.swift
@@ -0,0 +1,18 @@
+//
+//  Spinner.swift
+//  intra42
+//
+//  Created by Mickaël on 18/08/2024.
+//
+
+import SwiftUI
+
+struct Spinner: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Spinner()
+}
